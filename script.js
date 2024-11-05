24bc59d13fdf517e623dcb4fc01fe49636fd65a0
Sinega-E <sinega.elanthiraiyan@gmail.com>
2024-11-05 18:55:48 +0530

    commit

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 'use strict';
 const modal=document.querySelector('.modal');
 const overlay=document.querySelector('.overlay');
-const btnCloseModal=document.querySelector('.close-modal');
+const btnsCloseModal=document.querySelector('.close-modal');
 const btnsOpenModel=document.querySelectorAll('.show-modal');
-console.log(btnsOpenModel)
+
+const openmodal=function(){
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+    };
+const closemodal=function(){
+        modal.classList.add('hidden')
+        overlay.classList.add('hidden')
+    };
 for(let i=0;i<btnsOpenModel.length;i++)
-    console.log(btnsOpenModel[i].textContent)
+    btnsOpenModel[i].addEventListener('click',openmodal);
+
+// for closing the modal
+btnsCloseModal.addEventListener('click',closemodal);
+overlay.addEventListener('click',closemodal);
+
+// for escape key
+document.addEventListener('keydown',function(e){
+    if(e.key==='Escape' && !modal.classList.contains('hidden')){
+        closemodal();
+    }
+})
+
+
+
+
